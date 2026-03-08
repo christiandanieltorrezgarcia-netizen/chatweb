7edc398ffdf06a4c381f6f1a50849fbb1a00c653
Uri50728 <theuri50728@gmail.com>
2026-03-08 12:00:51 -0600

    Correccion en las rutas

--- ChuchoGram/routes/web.php
+++ ChuchoGram/routes/web.php
@@ -5,23 +5,23 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 use App\Http\Controllers\MessageController;
 
-Route::get('/', [AuthenticatedSessionController::class, 'create'])->name('login');
+Route::get('/', function () {
+    return auth()->check()
+        ? redirect()->route('chat')
+        : redirect()->route('login');
+});
 
 Route::get('/chat', [MessageController::class, 'index'])->middleware('auth')->name('chat');
 Route::post('/chat', [MessageController::class, 'store'])->middleware('auth');
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+    return redirect()->route('chat');
+})->middleware(['auth'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
 });
 
 require __DIR__.'/auth.php';
